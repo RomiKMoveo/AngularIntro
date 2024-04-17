d321804f961221cd7391a943ab7f5f495ab61322
romi kamin <romik@moveo.co.il>
2024-04-17 13:34:09 +0300

    Practicing Directives Assignment udemy

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrl: '../app.component.css'
 })
 export class ServerComponent {
+  showSecret = false;
+  log = [];
 
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
