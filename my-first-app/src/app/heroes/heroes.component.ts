ea0c19be2c0f1e5bfce03ad843d30b5f1570f40c
romi kamin <romik@moveo.co.il>
2024-04-18 09:49:03 +0300

    display list of heroes with user-selectable functionality to view hero details upon selection

--- my-first-app/src/app/heroes/heroes.component.ts
+++ my-first-app/src/app/heroes/heroes.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { NgIf, NgForOf, UpperCasePipe } from '@angular/common'; // Import specific Angular directives and pipes
+import { FormsModule } from '@angular/forms';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  providers: [UpperCasePipe], // You can provide UpperCasePipe here if needed
 })
+
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
